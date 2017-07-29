96ed0e66a8db25502218907419b300d77d419b68
Alex Pavlovic <lex.pavlovic@gmail.com>
2017-07-29 10:03:49 +0200

    Initial Files

---
+++ UP/E01/E01.js
@@ -0,0 +1,14 @@
+const TAX_RATE = 0;
+const PHONE_PRICE = 0;
+const ACCESSORY_PRICE = 0;
+const SPENDING_TRESHOLD = 0;
+
+var bankAccountBalance = 0;
+
+bankaccountBalance = prompt("Please enter your Bank Account Balance:");
+
+
+function calculateTax();
+
+function formatPrice();
+
