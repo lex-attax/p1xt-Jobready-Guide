1a31570bb30d9139a74c1ffae1e9ad64c73cd147
Alex Pavlovic <lex.pavlovic@gmail.com>
2017-08-19 14:33:44 +0200

    callSite example for 'this'

---
+++ YDKJS_ThisAndObjectPrototypes/callSite.js
@@ -0,0 +1,30 @@
+function baz() {
+	//call-stack is:'baz'
+	//so, our call-site is in the global scope
+	console.log("baz");
+	bar(); // <-- call-sit for 'bar'
+
+}
+
+function bar() {
+	// call-stack is: 'baz' -> 'bar'
+	//so, our call-site is in 'baz'
+	
+	console.log("bar");
+	foo(); //<-- call-site for 'foo'
+	
+}
+
+function foo() {
+	// call-stack is: 'baz' ->'bar' ->'foo'
+	//so, our call-site is in 'bar'
+	
+	console.log("foo");
+	
+}
+
+bar(); //<-- call-sit for 'baz'
+
+//Take care when analyzing code to find the actual call-site(form the call-stack),
+// because it's the only thing that matters for 'this'-binding.
+	
